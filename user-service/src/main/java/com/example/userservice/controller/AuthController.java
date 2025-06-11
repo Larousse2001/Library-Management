bffc74c35773f0d9d016960e90128149e5af474b
Arous Achraf <87370368+Larousse2001@users.noreply.github.com>
2025-06-11 21:25:47 +0100

    Add files via upload

---
+++ user-service/src/main/java/com/example/userservice/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.*;
+import com.example.userservice.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
+
---
+++ user-service/src/main/java/com/example/userservice/dto/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.example.userservice.dto;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+}
+
