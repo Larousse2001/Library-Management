198c6e355877c4bf8805afc14bbbec6cbabb6c12
Arous Achraf <87370368+Larousse2001@users.noreply.github.com>
2025-06-11 21:21:28 +0100

    Delete library-front-react directory

--- library-front-react/src/services/searchService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8084/api/search'; // search-service
-
-export const searchByTitle = (title) => axios.get(`${API_URL}/title?title=${title}`);
-export const searchByAuthor = (author) => axios.get(`${API_URL}/author?author=${author}`);
-export const searchByCategory = (category) => axios.get(`${API_URL}/category?category=${category}`);
